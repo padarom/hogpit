136637efa42c8dcefaad12a2a3e7fbc13ace0390
Christopher Mühl <christopher@padarom.xyz>
2022-07-24 19:51:36 +0200

    Add stuff

--- code/instruments/adi/src/main.cpp
+++ code/instruments/adi/src/main.cpp
@@ -1,49 +1,15 @@
-#include <Arduino.h>
-#include <FB5317M.h>
+// ADI is bus device #1
+#define DCSBIOS_RS485_SLAVE 1
+#define TXENABLE_PIN 2
 
-FB5317M xAxis;
-FB5317M yAxis;
+#include <DcsBios.h>
+
+DcsBios::PotentiometerEWMA<5, 128, 5> stallVol("STALL_VOL", A0);
 
 void setup() {
-    Serial.begin(9600);
-    xAxis.attach(9, 4);
-    yAxis.attach(6, 4);
-    // xAxis.calibrate();
-    
-    // xAxis.seek(PI);
-    // yAxis.seek(PI);
+    DcsBios::setup();
 }
 
-
-unsigned int count = 0;
-
 void loop() {
-    xAxis.seek(2000);
-    yAxis.seek(1500);
-
-    delay(2000);
-
-    xAxis.seek(1500);
-    yAxis.seek(2000);
-
-    delay(2000);
-
-    xAxis.seek(1600);
-    yAxis.seek(1600);
-
-    delay(5000);
-
-    xAxis.seek(1700);
-    yAxis.seek(1300);
-
-    delay(10000);
-
-    /*
-    delay(1);
-    count++;
-    if (count > 3000) {
-      xAxis.seek(-xAxis.getSeekingAngle());
-      count = 0;
-    }
-    */
+    DcsBios::loop();
 }
--- code/scorpion/src-tauri/src/enumerate.rs
+++ code/scorpion/src-tauri/src/enumerate.rs
@@ -5,14 +5,11 @@ use std::collections::HashSet;
 fn match_info(device: &Device, info_param: Rs2CameraInfo) -> String {
     match device.info(info_param) {
         Some(s) => String::from(s.to_str().unwrap()),
-        None => String::from("N/A"),
+        None => String::from("N/A")
     }
 }
 
 fn main() -> Result<()> {
-    println!("----\nEnumerating all devices compatible with RealSense:\n----");
-    // The below code is the equivalent of creating a HashSet of query_devices that just contains Rs2ProductLine::Any.
-    // In other words, this will look for any connected device that is compatible with RealSense.
     let devices = Context::new()?.query_devices(HashSet::new());
     ensure!(!devices.is_empty(), "No devices found");
 
